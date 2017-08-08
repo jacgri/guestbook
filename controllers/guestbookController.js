138d780cb1f44765c5e22b2a3b463ee2d43e3a1e
Jacqui Grimes <grimesjacqui@gmail.com>
2017-08-08 19:58:37 +0100

    adding and deleting comments to a guest book

--- controllers/guestbookController.js
+++ controllers/guestbookController.js
@@ -1,10 +1,27 @@
+var bodyParser = require('body-parser')
 var guestbook = require('../models/guestbook.js')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 function guestbookController (app) {
     app.get('/', function (req, res) {
         res.render('home', {
             guestbook: guestbook.getItems()
         })
+    })
+
+    app.post('/', urlencodedParser, function (req, res) {
+        guestbook.addItem(req.body.name, req.body.comment)
+            res.render('home', {
+                guestbook: guestbook.getItems()
+            })
+    })
+
+    app.delete('/', urlencodedParser, function (req, res) {
+          guestbook.removeItem(req.body.id)
+            res.render('home', {
+                guestbook: guestbook.getItems()
+            })
     })
 }
 
--- models/guestbook.js
+++ models/guestbook.js
@@ -9,19 +9,25 @@ Guestbook.prototype = {
     return this._items
   },
 
-    addItem: function(name,comment){
+    addItem: function(name, comment){
         this._items.push({
             id: randomID(),
             name: name,
-            comment:comment
+            comment: comment
         })
+    },
+
+    removeItem: function(id){
+        this._items = this._items.filter(function (item) {
+            return item.id !== id
+    })
     }
 }
 
 var guestbook = new Guestbook()
 
-guestbook.addItem('Jacqui', 'Great visit')
+/*guestbook.addItem('Jacqui', 'Great visit')
 guestbook.addItem('James', 'Didnt enjoy my visit')
-guestbook.addItem('Rachel', 'Loved it')
+guestbook.addItem('Rachel', 'Loved it')*/
 
-module.export = guestbook
+module.exports = guestbook
